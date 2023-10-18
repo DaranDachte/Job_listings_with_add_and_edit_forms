d8b8d187785c86f9f78b6e02d45cb49a5d5cf8b7
Daran Dachte <darandachte@gmail.com>
2023-10-18 23:51:15 +0200

    delete button added

--- src/Components/Container/Content/List/Item/Tags/Tags.tsx
+++ src/Components/Container/Content/List/Item/Tags/Tags.tsx
@@ -2,7 +2,7 @@ const Tags = ({ tags }) => {
   return (
     <div className=" h-[2rem]">
       {tags.map((tag) => (
-        <span className=" hover:text-[#FFF] rounded bg-[#eef7f6] text-[#5CA5A5] p-1 mx-1 hover:bg-[#5CA5A5]  tracking-[-0.00769rem]  hover:cursor-pointer ">
+        <span className="hover:text-[#FFF] rounded bg-[#eef7f6] text-[#5CA5A5] p-1 mx-1 hover:bg-[#5CA5A5]  tracking-[-0.00769rem]  hover:cursor-pointer ">
           {tag}
         </span>
       ))}
